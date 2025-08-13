867846fea4400704d0115d22a49d1201bde0229c
Kushagra Goyal <kushagragoyal13@gmail.com>
2025-08-13 17:46:59 +0530

    day1

---
+++ Day1_Variables.py
@@ -0,0 +1,29 @@
+# 1. Create variables for your name, age, and monthly income. Print them in a sentence.
+
+name="Kushagra"
+age=22
+monthly_income=50000
+# print("My name is ",name,"and my age is",age, "and I'm earning monthly",monthly_income)
+
+
+# 2.Convert your monthly income (int) into a string and print its type.
+
+income=str(monthly_income)
+# print(income,type(income))
+
+
+# 3.Create two float numbers and add them together.
+
+num1=2.5 
+num2=2.6
+# print(num1+num2)
+
+# 4.Store your age as a string, convert it to an integer, and add 5 to it.
+
+age_str="22"
+age_int=int(age_str)
+# print(age_int+5)
+
+# 5.Print "Hello" 5 times without writing it 5 times manually.
+
+print("hello"*5)
